3ca92cefa9d8f4a332fced2d27cfc27f13b0087c
Juno Nguyen <junonguyen27@gmail.com>
2022-07-14 17:27:51 -0500

    adding solved 7-12

--- 11-Express/01-Activities/01-API-Endpoints/Unsolved/server.js
+++ 11-Express/01-Activities/01-API-Endpoints/Unsolved/server.js
@@ -1,7 +1,4 @@
 // TODO: Import express
-const express = require('express');
-const db = require('./terms.json');
-const app = express();
 
 // TODO: Import 'terms.json' file
 const PORT = 3001;
@@ -16,9 +13,6 @@ app.get('/html', (req, res) => {
 });
 
 // TODO: Create a route for a GET request that will return the content of our `termData.json` file
-app.get('/api/', (req, res) => {
-  res.json(db)
-});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
--- 11-Express/01-Activities/03-POST-Requests/Unsolved/server.js
+++ 11-Express/01-Activities/03-POST-Requests/Unsolved/server.js
@@ -7,26 +7,18 @@ const app = express();
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
 app.get('/api/reviews', (req, res) => {
   // Your code here
-  res.json(`Request to get reviews`)
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
 // Your code here
-app.post('/api/reviews', (req, res) => {
-  res.json(`Post request to post reviews`)
-})
 
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
 app.get('/api/upvotes', (req, res) => {
   // Your code here
-  res.json(`Request to get upvotes made`)
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
 // Your code here
-app.post('/api/upvotes', (req, res) => {
-  res.json(`Post request to post upvotes`)
-})
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
